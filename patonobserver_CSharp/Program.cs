56e20cc3af869cfa9b2117b25d5452cde5e0b05a
kevingenerico <66702623+kevingenerico@users.noreply.github.com>
2020-07-05 17:41:53 -0400

    Add files via upload

---
+++ patonobserver_CSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace patonobserver_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creo usuarios
+            Usuario user1 = new Usuario("luis1535");
+            Usuario user2 = new Usuario("matiasblog");
+            Usuario user3 = new Usuario("pofesorjuan");
+            Usuario user4 = new Usuario("joseplay");
+
+            // Los subscribo 
+            user1.subscribirseA(user4);
+            user2.subscribirseA(user4);
+            user3.subscribirseA(user4);
+            user4.subscribirseA(user3);
+
+            // Subo video
+            user4.subirVideo("la mejor partida de fornite");
+            Console.WriteLine("");
+            user3.subirVideo("cusro de angular 10");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ patonobserver_CSharp/Usuario.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace patonobserver_CSharp
+{
+    class Usuario
+    {
+
+        private string username;
+        private List<Usuario> subscriptores = new List<Usuario>();
+
+        public Usuario(string username)
+        {
+            this.username = username;
+        }
+
+        public void subirVideo(string nombreVideo)
+        {
+            foreach (Usuario usuario in subscriptores)
+            {
+                usuario.recibirAviso(this.username, nombreVideo);
+            }
+
+        }
+
+        public void recibirAviso(string usuario, string nombreVideo)
+        {
+            Console.WriteLine("oye "+ this.username + " sabes que " + usuario + " ha subido un video. Titulo: " + nombreVideo);
+        }
+
+        public void subscribirseA(Usuario usuario)
+        {
+            usuario.añadirSubscriptor(this);
+        }
+
+        public void desubscribirseA(Usuario usuario)
+        {
+            usuario.quitarSubscriptor(this);
+        }
+
+        protected void añadirSubscriptor(Usuario usuario)
+        {
+            this.subscriptores.Add(usuario);
+        }
+
+        protected void quitarSubscriptor(Usuario usuario)
+        {
+            this.subscriptores.Remove(usuario);
+        }
+    }
+}
