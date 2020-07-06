ac0c733ac2102bd73ed29055628de31138dd8bfd
kevingenerico <66702623+kevingenerico@users.noreply.github.com>
2020-07-05 21:44:23 -0400

    Add files via upload

---
+++ patronsingleton_CSharp/Clase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace patronsingleton_CSharp
+{
+    class Clase
+    {
+        private static Clase instance = null;
+        public string mensaje = "";
+
+        protected Clase()
+        {
+            mensaje = "Dios le bendiga";
+        }
+
+        public static Clase Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Clase();
+
+                return instance;
+            }
+        }
+    }
+}
---
+++ patronsingleton_CSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace patronsingleton_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Clase.Instance.mensaje);
+            Clase.Instance.mensaje = "\nDios le siga bendiciendo";
+            Console.WriteLine(Clase.Instance.mensaje);
+            Console.ReadKey();
+        }
+    }
+}
